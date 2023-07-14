ea1bfb2b6a16bcac682b12659d9ea2900a8efa92
dave <hsg829@gmail.com>
2023-07-14 18:45:54 +0900

    HTML, CSS - edited 07/14

--- src/type.js
+++ src/type.js
@@ -1,22 +1,22 @@
-'use strict';
+"use strict";
 
-new TypeIt('.home__title--strong', {
+new TypeIt(".home__title--strong", {
   loop: true,
   speed: 100,
-}) // Dream Coder|
-  .move(-11)
-  .type('Amazing ') //Amazing |Dream Coder
+})
+  .move(-15)
+  .type("Amazing ") //Amazing |Dream Coder
   .pause(1000)
-  .move(null, { to: 'END' }) //Amazing Dream Coder|
+  .move(null, { to: "END" }) //Amazing Dream Coder|
   .delete() //|
-  .type('Front-end Engineer') //Front-end Engineer|
+  .type("Front-end Engineer") //Front-end Engineer|
   .pause(1000)
   .move(-9) //Front-end| Engineer
   .delete(9) //| Engineer
-  .type('Back-end') //Back-end| Engineer
+  .type("Back-end") //Back-end| Engineer
   .pause(1000)
   .delete(8) //| Engineer
-  .type('Full-stack') //Full-stack| Engineer
+  .type("Full-stack") //Full-stack| Engineer
   .pause(1000)
   .move(9) //Full-stack Engineer|
   .delete()
